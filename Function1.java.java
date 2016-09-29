6845e3adb6c0522bc7b151c894b8a06370e0505a
ChenMaksim <chen.maksim@mail.ru>
2016-09-29 18:58:03 +0300

    Add files via upload

---
+++ Function1.java.java
@@ -0,0 +1,16 @@
+public class Function1 {
+    public static void main(String[] args){
+        int [] a = {23, 11, 150, 7};
+        int max = a[0];
+        int min = a[0];
+        for(int i = 0; i != a.length; i ++){
+            if(a[i] > max){
+                max = a[i];
+            }
+            if(a[i] < min){
+                min = a[i];
+            }
+        }
+        System.out.println(min + " " + max);
+    }
+}
---
+++ removing.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+ public class removing{
+  public static void main(String[]args){
+     int a[]={-3,-7,10,11,2};
+      Scanner scan = new Scanner(System.in);
+       int b = scan.nextInt();
+       for(int i=b;i < a.length-1;i++){
+        a[i]=a[i+1];}
+         for(int i=0;i<a.length - 1;i++){
+           System.out.println(a[i]+" ");
+    }
+  }
+}
